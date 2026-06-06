e0059f7f0f6e6afa6af59a43cb669bae21108954
unknown <amirwebsajjad@gmail.com>
2026-06-06 15:12:18 +0330

    first-update

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['images.unsplash.com', 'randomuser.me'], // در صورت استفاده از عکس‌های خارجی
-  },
 };
 
 export default nextConfig;
